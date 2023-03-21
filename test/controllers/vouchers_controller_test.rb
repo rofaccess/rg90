815ecc826cf00917b37c21ff423660d20d66e87f
Rodrigo <rofaccess@gmail.com>
2023-03-21 20:00:44 -0300

    Se mejora la estructura de pruebas de la acción create. Se reordena los asserts de las acciones index y new

--- test/controllers/vouchers_controller_test.rb
+++ test/controllers/vouchers_controller_test.rb
@@ -8,6 +8,10 @@ class VouchersControllerTest < ActionDispatch::IntegrationTest
   describe "GET index" do
     before { get vouchers_url }
 
+    should "load all vouchers into @vouchers" do
+      assert_equal Voucher.all.size, assigns(:vouchers).size
+    end
+
     should "responds successfully with an HTTP 200 status code" do
       assert_response :success
     end
@@ -15,14 +19,14 @@ class VouchersControllerTest < ActionDispatch::IntegrationTest
     should "renders the index template" do
       assert_template :index
     end
-
-    should "load all vouchers into @vouchers" do
-      assert_equal Voucher.all.size, assigns(:vouchers).size
-    end
   end
 
   describe "GET new" do
     before { get new_voucher_url }
+
+    should "load a new voucher into @voucher" do
+      assert assigns(:voucher).new_record?
+    end
 
     should "responds successfully with an HTTP 200 status code" do
       assert_response :success
@@ -31,18 +35,45 @@ class VouchersControllerTest < ActionDispatch::IntegrationTest
     should "renders the new template" do
       assert_template :new
     end
-
-    should "load a new user into @voucher" do
-      assert assigns(:voucher).new_record?
-    end
   end
 
-  test "should create voucher" do
-    assert_difference("Voucher.count") do
-      post vouchers_url, params: { voucher: { business_identifier: @voucher.business_identifier, business_name: @voucher.business_name, date: @voucher.date, number: @voucher.number, timbrado: @voucher.timbrado, total_amount: @voucher.total_amount } }
+  describe "POST create" do
+    before do
+      @vouchers_count = Voucher.count
+
+      voucher_params = {
+        business_identifier: @voucher.business_identifier,
+        business_name: @voucher.business_name,
+        date: @voucher.date,
+        number: @voucher.number,
+        timbrado: @voucher.timbrado,
+        total_amount: @voucher.total_amount
+      }
+
+      post vouchers_url, params: { voucher: voucher_params }
     end
 
-    assert_redirected_to voucher_url(Voucher.last)
+    context "with valid params" do
+      should "create a new voucher" do
+        assert_equal @vouchers_count + 1, Voucher.count
+      end
+
+      should "load the new voucher into @voucher" do
+        assert assigns(:voucher)
+      end
+
+      should "responds successfully with an HTTP 302 status code" do
+        assert_response :found
+      end
+
+      should "redirects to the created new voucher" do
+        assert_redirected_to voucher_url(assigns(:voucher))
+      end
+    end
+
+    context "with invalid params" do
+
+    end
   end
 
   test "should show voucher" do
